a952658ffc558614e61e9e68a9d79d83be3e6a7e
Minish144 <varlamow.col@yahoo.com>
2020-03-26 14:38:07 +0300

    Update logic , mainwindow
    Доделал интерфейс
    Написал функции для чтения csv файла и его обработки

--- logic.cpp
+++ logic.cpp
@@ -1,1 +1,51 @@
 #include "logic.h"
+
+int checkFile(string filepath)
+{
+    ifstream file(filepath);
+    if (file.is_open())
+    {
+        qDebug() << "Все ОК! Файл удалось открыть!\n" << endl;
+        return 1;
+    }
+    else
+    {
+        qDebug() << "Файл не удалось открыть!\n" << endl;
+        return 0;
+    }
+}
+
+vector<string> readCSV(string filepath)
+{
+    checkFile(filepath);
+    ifstream file(filepath);
+    string tmp;
+    vector<string> sArray;
+    int count = 0;
+    for (int i = 0; getline(file, tmp); i++)
+    {
+        sArray.push_back(tmp);
+        count++;
+    }
+    for (int i = 0; i  < count; i++)
+    {
+        qDebug() << QString::fromStdString(sArray[i]);
+    }
+    return sArray;
+}
+
+
+vector<string> splitString(string str, string sep)
+{
+    vector<string> arr;
+    size_t prev = 0;
+    size_t next;
+    size_t delta = 1;
+    while ((next = str.find(sep, prev)) != string::npos)
+    {
+        arr.push_back(str.substr(prev, next - prev));
+        prev = next + delta;
+    }
+    arr.push_back(str.substr(prev));
+    return arr;
+}
--- logic.h
+++ logic.h
@@ -1,11 +1,27 @@
 #ifndef LOGIC_H
 #define LOGIC_H
 
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMainWindow>
+#include <QFileDialog>
 
-class logic
-{
-public:
-    logic();
-};
+#include <stdio.h>
+#include <QString>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <QDebug>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int checkFile(string filepath);
+
+vector<string> readCSV(string filepath);
+
+vector<string> splitString(string str, string sep);
+
 
 #endif // LOGIC_H
